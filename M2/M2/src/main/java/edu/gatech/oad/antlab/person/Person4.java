b1dd3819bc8898ae6519c97b651b0f8ba7173a34
halequin <halequin3@gatech.edu>
2016-01-29 12:50:01 -0500

    Updated Calc Method

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -1,27 +1,33 @@
 package edu.gatech.oad.antlab.person;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.ArrayList;
+
 /**
  *  A simple class for person 4
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Hannah Alequin
  *  @version 1.1
  */
 public class Person4 {
-  /** Holds the persons real name */
+    /** Holds the persons real name */
   private String name;
     /**
      * The constructor, takes in the persons
      * name
      * @param pname the person's real name
      */
+
+
   public Person4(String pname) {
     name = pname;
   }
     /**
      * This method should return a string
-     * where each character is 1 greater 
+     * where each character is 1 greater
      * than its previous value.  So
      * given "abc123" it should return
      * "bcd234".
@@ -31,15 +37,19 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+        char[] letters = input.toCharArray();
+      for (int i = 0; i < letters.length; i++) {
+          letters[i] = (letters[i])+=1;
+      }
+        return new String(letters);
     }
-    
+
     /**
      * Return a string rep of this object
      * that varies with an input string
      *
      * @param input the varying string
-     * @return the string representing the 
+     * @return the string representing the
      *         object
      */
     public String toString(String input) {
